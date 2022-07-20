747a3497d71547c5fdba1ee7b613e6b0a3991159
Suryansh Tokas <suryansh.tokas@gmail.com>
2022-07-20 17:47:05 +0530

    Added Upcoming Repacks, Help, Reply to mention functionality, embedded all commands

--- discord_bot.py
+++ discord_bot.py
@@ -19,15 +19,35 @@ async def on_ready():
 async def on_message(message):
     if message.author ==  client.user:
         return
+    
+    if client.user.mentioned_in(message):
+        await message.channel.send("Use $repack help for more information")
 
-    if message.content.startswith("$repack"):
+    elif message.content.startswith("$repack help"):
+        embed = discord.Embed(title="Help", description="List of commands for the bot", color=0x00FF00)
+        embed.add_field(name="$repack [name_of_game]", value="To show all games avaiable on fitgirl's site with the name", inline=False)
+        embed.add_field(name="$repack upcoming", value="Show upcoming repacks", inline=False)
+        await message.channel.send(embed=embed)
+
+    elif message.content.startswith("$repack upcoming"):
+        await message.channel.send("Finding ...")
+        upcoming_results = main.upcoming_repacks()
+        embed = discord.Embed(title="Upcoming Repacks", description="get a list of upcoming repacks")
+        for i, j in enumerate(upcoming_results):
+            embed.add_field(name=i, value=j, inline=False)
+        await message.channel.send(embed=embed)
+
+    elif message.content.startswith("$repack"):
         await message.channel.send("Finding ...")
         message_content = message.content[8:]
         first_result = main.main(message_content)
         if len(first_result) == 0:
             await message.channel.send("Not Available :(")
         else:
-            await message.channel.send(first_result)
+            embed = discord.Embed(title="Results", description="List of games available on fitgirl repacks ")
+            for i in first_result:
+                embed.add_field(name=i[0], value=i[1], inline=False)
+            await message.channel.send(embed=embed)
 
 
 client.run(TOKEN)
--- main.py
+++ main.py
@@ -15,10 +15,29 @@ def main(game_name):
     results = driver.find_elements(By.XPATH, "//h1/a")
     results.pop(0)
 
-    results_final = ""
+    results_list = []
     for i in results:
-        results_final += i.text
-        results_final += "\n" + str(i.get_attribute("href")) + "\n\n"
+        results_final = [] 
+        results_final.append(i.text)
+        results_final.append(str(i.get_attribute("href")))  
+        results_list.append(results_final)
     driver.quit()
-    return results_final 
+    return results_list
+
+
+def upcoming_repacks():
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.common.keys import Keys
+
+    driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+
+    driver.get("https://fitgirl-repacks.site/")
+
+    upcoming_games_raw = driver.find_elements(By.XPATH, "//h3/span")
+    upcoming_games = []
+    for i in upcoming_games_raw:
+        upcoming_games.append(i.text)
+    driver.quit()
+    return upcoming_games[:-10]
 
